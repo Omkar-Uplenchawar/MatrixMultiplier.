376dae211244cc82d2a286282fa27cd284aa49f5
Omkar Santosh Uplenchawar <89998258+Omkar-Uplenchawar@users.noreply.github.com>
2023-12-27 03:12:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MatrixMultiplier from './MatrixMultiplier'
+
+const App = () => {
+  return (
+    <>
+      <MatrixMultiplier/>
+    </>
+  )
+}
+
+export default App
